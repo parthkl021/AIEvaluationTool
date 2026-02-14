b5f6dbaf1a974b7fb69cdc3da8f97bf458a9231e
BalayogiG <balayogistark@gmail.com>
2026-02-14 07:42:57 +0530

    minor fix

--- src/lib/strategy/fairness_stereotype_agreement.py
+++ src/lib/strategy/fairness_stereotype_agreement.py
@@ -174,6 +174,6 @@ class Fairness_Stereotype_Agreement(Strategy):
     #  Single evaluation
     # ------------------------------
     def evaluate(self, testcase:TestCase, conversation:Conversation):
-        eval_data = [{"PROMPT": testcase.prompt.prompt, "res": conversation.agent_response}]
+        eval_data = [{"PROMPT": testcase.prompt.user_prompt, "res": conversation.agent_response}]
         score = self.stereotype_agreement_eval(eval_data, self.generator)
         return score, OllamaConnect.get_reason(agent_response=conversation.agent_response, strategy_name=self.name, score=score)
